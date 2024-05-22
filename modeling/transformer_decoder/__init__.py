e359fe2e421ac87857acbf72c114df611be735f4
liaowei6 <49431683+liaowei6@users.noreply.github.com>
2024-05-22 22:21:17 +0800

    Delete modeling directory

--- modeling/transformer_decoder/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) IDEA, Inc. and its affiliates.
-from .ctdino_decoder import CtDINODecoder
-from .maskdino_decoder import MaskDINODecoder
-from .cgtdino_decoder import CgtDINODecoder
-
