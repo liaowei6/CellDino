24d7e313e2cb66c3166f044551ceb681602d770d
liaowei6 <49431683+liaowei6@users.noreply.github.com>
2024-05-22 22:48:01 +0800

    Add files via upload

---
+++ maskdino/modeling/transformer_decoder/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) IDEA, Inc. and its affiliates.
+from .ctdino_decoder import CtDINODecoder
+from .maskdino_decoder import MaskDINODecoder
+from .cgtdino_decoder import CgtDINODecoder
+
